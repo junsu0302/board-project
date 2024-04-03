f170449b6d49724392b4768eb01d3806c4c457c4
junsu0302 <junsu96378224@gmail.com>
2024-04-03 14:25:09 +0900

    #7 - 게시판 View 기본 골격 작성
    게시판 서비스 뷰를 만들고, 엔드포인트 기초 테스트를 작성하고, 디자인 요소로 부트스트랩을 적용했다.
    
    테스트에서 타입 에러가 발생하여 부가 타입까지 유연하게 대처 가능하게 변경

--- src/main/java/com/project/board/Controller/ArticleController.java
+++ src/main/java/com/project/board/Controller/ArticleController.java
@@ -1,9 +1,19 @@
 package com.project.board.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/articles") // 전역 경로 설정
 @Controller
 public class ArticleController  {
+
+    @GetMapping // Controller에서 View로 데이터 전달
+    public String articles(ModelMap map) {
+        map.addAttribute("articles", List.of()); // View에서 활용할 리스트 생성
+        return "articles/index"; // 해당 이름의 View 반환
+    }
 }

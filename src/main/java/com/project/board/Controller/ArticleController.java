9be7fdf950a6d49d23c430719c230060c73cb8d9
junsu0302 <junsu96378224@gmail.com>
2024-04-03 13:04:49 +0900

    #7 - 게시글 관련 View 테스트 작성
    어떤 경로에서 View를 열어줄지 테스트를 통해 명세 작성
    
    테스트의 경우 데이터가 없어서 테스트와 gradle build가 실패하여 배포 자동화에 악영향을 미칠 수 있다.
    
    따라서 Disable 처리를 했다.

---
+++ src/main/java/com/project/board/Controller/ArticleController.java
@@ -0,0 +1,9 @@
+package com.project.board.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles") // 전역 경로 설정
+@Controller
+public class ArticleController  {
+}

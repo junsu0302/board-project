ee1f1e9e7c2523ae466ce98e6af86b71d6e85ce3
junsu0302 <junsu96378224@gmail.com>
2024-04-06 21:32:22 +0900

    #12 - 리팩토링 : 세부 개발 설정 변경
    root 페이지를 articles 페이지로 변경하여 테스트 중 작성해야하는 url 길이 단축
    
    jpa 세부 설정이 들어있는 디렉토리를 gitignore에 추가
    
    userAccount가 포함된 데이터로 변경
    
    OSIV 방지를 위한 application.yaml 설정 추가

--- src/main/java/com/project/board/Controller/MainController.java
+++ src/main/java/com/project/board/Controller/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
     @GetMapping("/")
     public String root() {
-        return "redirect:/articles";
+
+        return "forward:/articles";
     }
 }

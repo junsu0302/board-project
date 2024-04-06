569221ae1d0e75779c12f0b67e8fed06874dd26c
junsu0302 <junsu96378224@gmail.com>
2024-04-06 16:27:44 +0900

    #12 - 메인 페이지 설정
    게시판 페이지를 메인 페이지로 설정 및 테스트 작성

---
+++ src/test/java/com/project/board/controller/MainControllerTest.java
@@ -0,0 +1,39 @@
+package com.project.board.controller;
+
+import com.project.board.Controller.MainController;
+import com.project.board.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Import(SecurityConfig.class)
+@DisplayName("View Controller - 메인페이지")
+@WebMvcTest(MainController.class)
+class MainControllerTest {
+    // 테스트 타겟
+    private final MockMvc mvc;
+
+    public MainControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    // 테스트
+    @DisplayName("[view][GET] 루트 페이지 테스트")
+    @Test
+    void givenNothing_whenRequestingRootPage_thenRedirectsToArticlesPage() throws Exception {
+        // Given
+
+
+        // When & Then
+        mvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection());
+
+    }
+}

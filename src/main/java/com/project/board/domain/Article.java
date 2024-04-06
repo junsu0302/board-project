7b60b7fb5378796927687e1b2da874a6d5dca9d0
junsu0302 <junsu96378224@gmail.com>
2024-04-06 14:24:41 +0900

    #9 - 리펙토링 : userAccount 사용
    Article, ArticleComment에 userAccount 객체를 사용
    
    추가로 안쓰는 테스트와 userAccount 사항을 data.sql에 추가
    
    메인 View 페이지 이동을 article로 변경

--- src/main/java/com/project/board/domain/Article.java
+++ src/main/java/com/project/board/domain/Article.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 import java.util.Set;
 
 @Getter // 모든 필드에 접근 가능하게 설정
-@ToString // 쉽게 출력 가능하게 설정
+@ToString(callSuper = true) // 쉽게 출력 가능하게 설정
 @Table(indexes = { // 쉽게 관찰 가능하게 설정
         @Index(columnList = "title"),
         @Index(columnList = "hashtag"),
@@ -23,29 +23,30 @@ public class Article extends AuditingFields {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 설정
     private Long id; // 아이디
 
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount; // 유저 정보 (ID)
     @Setter @Column(nullable = false) private String title; // 제목 (수정 가능)
     @Setter @Column(nullable = false, length = 10000) private String content; // 본문 (수정 가능, 10000자)
 
     @Setter private String hashtag; // 해시태그 (수정 가능)
 
     @ToString.Exclude // 순환 참조 발생 시 연결 해제
-    @OrderBy("id")
+    @OrderBy("createdAt DESC")
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>(); // 양방향 바인딩
 
     // 기능
     protected Article() {} // 기본 생성자
 
-    private Article(String title, String content, String hashtag) {
+    private Article(UserAccount userAccount, String title, String content, String hashtag) {
         // 도메인 Article 생성할 때, 필요한 정보 세팅
+        this.userAccount = userAccount;
         this.title = title;
         this.content = content;
         this.hashtag = hashtag;
     }
 
-    public static Article of(String title, String content, String hashtag) {
-        // 사용하기 편하게 세팅
-        return new Article(title, content, hashtag);
+    public static Article of(UserAccount userAccount, String title, String content, String hashtag) {
+        return new Article(userAccount, title, content, hashtag);
     }
 
     @Override

cd29ed39ee40ecd5df71fed1be2932c9f2634531
junsu0302 <junsu96378224@gmail.com>
2024-04-08 14:10:58 +0900

    #14 - 게시판 Controller 구현
    게시판 기본 조회 화면을 위한 컨트롤러 구현하고 데이터 전달 완료

--- src/main/java/com/project/board/Controller/ArticleController.java
+++ src/main/java/com/project/board/Controller/ArticleController.java
@@ -1,26 +1,44 @@
 package com.project.board.Controller;
 
+import com.project.board.domain.type.SearchType;
+import com.project.board.dto.response.ArticleResponse;
+import com.project.board.dto.response.ArticleWithCommentsResponse;
+import com.project.board.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
+@RequiredArgsConstructor // 생성자 자동 생성
 @RequestMapping("/articles") // 전역 경로 설정
 @Controller
 public class ArticleController  {
+
+    private final ArticleService articleService;
+
     @GetMapping // Controller에서 View로 데이터 전달
-    public String articles(ModelMap map) {
-        map.addAttribute("articles", List.of()); // View에서 활용할 데이터
+    public String articles(
+            @RequestParam(required = false) SearchType searchType,
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            ModelMap map
+    ) {
+        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
         return "articles/index"; // 해당 이름의 View 반환
     }
 
     @GetMapping("/{articleId}") // Controller에서 View로 데이터 전달
     public String article(@PathVariable Long articleId, ModelMap map) {
-        map.addAttribute("article", "article"); // View에서 활용할 데이터 TODO: 구현할 때 실제 데이터 넣어야 함
-        map.addAttribute("articleComments", List.of()); // View에서 활용할 데이터
+        ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
+        map.addAttribute("article", article); // View에서 활용할 데이터
+        map.addAttribute("articleComments", article.articleCommentResponse()); // View에서 활용할 데이터
+
         return "articles/detail"; // 해당 이름의 View 반환
     }
 }
--- src/test/java/com/project/board/controller/ArticleControllerTest.java
+++ src/test/java/com/project/board/controller/ArticleControllerTest.java
@@ -2,15 +2,28 @@ package com.project.board.controller;
 
 import com.project.board.Controller.ArticleController;
 import com.project.board.config.SecurityConfig;
+import com.project.board.dto.ArticleWithCommentsDto;
+import com.project.board.dto.UserAccountDto;
+import com.project.board.service.ArticleService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -20,6 +33,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ArticleControllerTest {
     // 테스트 타겟 정의
     private final MockMvc mvc;
+
+    @MockBean private ArticleService articleService; // 연결을 끊어줌
 
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
@@ -31,6 +46,7 @@ class ArticleControllerTest {
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
         // Given
+        given(articleService.searchArticles(eq(null), eq(null), any(Pageable.class))).willReturn(Page.empty());
 
         // When & Then
         mvc.perform(get("/articles"))
@@ -38,20 +54,24 @@ class ArticleControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
+        then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
     }
 
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
         // Given
+        Long articleId = 1L;
+        given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
 
         // When & Then
-        mvc.perform(get("/articles/1"))
+        mvc.perform(get("/articles/" + articleId))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
+        then(articleService).should().getArticle(articleId);
     }
 
     @Disabled("구현 중")
@@ -80,4 +100,32 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles/search-hashtag"));
     }
 
+    private ArticleWithCommentsDto createArticleWithCommentsDto() {
+        return ArticleWithCommentsDto.of(
+                1L,
+                createUserAccountDto(),
+                Set.of(),
+                "title",
+                "content",
+                "#java",
+                LocalDateTime.now(),
+                "junsu",
+                LocalDateTime.now(),
+                "junsu"
+        );
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(1L,
+                "junsu",
+                "password",
+                "junsu@email.com",
+                "Junsu",
+                "memo",
+                LocalDateTime.now(),
+                "junsu",
+                LocalDateTime.now(),
+                "junsu"
+        );
+    }
 }
